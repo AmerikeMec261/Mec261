af0b11cc02edf8860c04453e29e1f0dd3c2f274c
Javier <jemilio1407@gmail.com>
2026-02-16 13:52:06 -0500

    Creacion del codigo
    Lever calculator code

---
+++ Mecanica261/Assets/levercalculator.cs
@@ -0,0 +1,10 @@
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using Unity.UI;
+using UnityEngine.UI;
+
+public class levercalculator : MonoBehaviour
+{
+    [Header("Dependencies")]
+    [SerializeField] private InputField _knowWeightOneInputField;
+}
