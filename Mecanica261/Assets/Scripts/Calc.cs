875c9f337287b4a105a76877cb2d7b46fe142947
Yormama <caceresdiego560@gmail.com>
2026-02-16 12:32:02 -0600

    UI creada con éxito

---
+++ Mecanica261/Assets/Scripts/Calc.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calc : MonoBehaviour
+{
+    public InputField inputD1;
+    public InputField inputM1;
+    public InputField inputD2;
+    public InputField inputM2;
+}
