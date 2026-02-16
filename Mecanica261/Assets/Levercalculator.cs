e8fe8c3836ed858278a8d9085147564a41a35f1f
Gael Saucedo <saucedo.gael1612@gmail.com>
2026-02-16 12:57:38 -0600

    Empezando codigo
    start code

---
+++ Mecanica261/Assets/Levercalculator.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine;
+using Unity.UI;
+using UnityEngine.UI;
+
+public class Levercalculator : MonoBehaviour
+{
+    [Header("Dependencies")]
+    [SerializeField] private InputField _knowWeightOneInputField;
+}
