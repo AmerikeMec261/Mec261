ef484f5c7a06e4650939eb73220c90e51d3318db
Wtflolxd37 <ju4n43rn4nde3z@gmail.com>
2026-02-16 12:55:25 -0600

    Mañana acabamos

---
+++ Mecanica261/Assets/Codigos/LevelCalculator.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelCalculator : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _knoWeigthOneInputField;
+    [SerializeField] private TMP_InputField _knomDistanceOneInputField;
+    [SerializeField] private TMP_InputField _knomWeigthTwoInputField;
+    [SerializeField] private TMP_InputField _knomDistanceTwoInputField;
+    [SerializeField] private TextMeshPro _distanceResult_Text; 
+}
