e2ed0603eab88521d5e5972b744623173d5017d2
DieguxTY <cdmx3047@amerike.edu.mx>
2026-02-16 12:54:38 -0600

    Codigo

--- Mecanica261/Assets/1er ejercicio/Masa.cs
+++ Mecanica261/Assets/1er ejercicio/Masa.cs
@@ -1,6 +1,14 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Masa : MonoBehaviour
 {
-  
+    [Header("Dependencies")]
+    [SerializeField] private InputField _knowWeightOneInputField_;
+    [SerializeField] private InputField _knowDistanceOneInputField_;
+    [SerializeField] private InputField _knowWeightTwoInputField_;
+    [SerializeField] private InputField _knowDistanceTwoInputField_;
+    [SerializeField] private TextMeshProUGUI _distanceResultado_;
+    [SerializeField] private TextMeshProUGUI _weightResultado_;
 }
