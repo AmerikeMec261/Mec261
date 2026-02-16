07f8be79e9d64bb9ef6cb9c4f81695050c786a6a
JulioRegalado <cdmx2939@amerike.edu.mx>
2026-02-16 12:51:58 -0600

    Calculadora terminada

---
+++ Mecanica261/Assets/scripts/reglade3.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class reglade3 : MonoBehaviour
+{
+    [Header("Dependencies")]
+
+    [SerializeField] private TMP_InputField _knoweightOneInputfield;
+    [SerializeField] private TMP_InputField _knoDistanceOneInputfield;
+    [SerializeField] private TMP_InputField _knoweightTwoInputfield;
+    [SerializeField] private TMP_InputField _knoDistanceTwoInputfield;
+    [SerializeField] private TextMeshProUGUI _distanceResultText;
+    [SerializeField] private TextMeshProUGUI _weightResultText;
+}
