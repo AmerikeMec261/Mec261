56da553472a4a0cc39d66e87526dfe0fec9ad610
SrT0RTUGA <cdmx2984@amerike.edu.mx>
2026-02-16 12:51:01 -0600

    FDGDFG

---
+++ Mecanica261/Assets/Levercalculator.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Unity.UI;
+using UnityEditor.Build.Reporting;
+using UnityEngine.UI;
+
+public class Levercalculator : MonoBehaviour
+{
+    [Header("Dependencies")]
+
+  
+}
