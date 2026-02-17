ba332edba6fe39c8017cafc7c4d33aed157c8f58
JulioRegalado <cdmx2939@amerike.edu.mx>
2026-02-17 12:18:29 -0600

    calculadora funcional guille

--- Mecanica261/Assets/scripts/reglade3.cs
+++ Mecanica261/Assets/scripts/reglade3.cs
@@ -7,9 +7,44 @@ public class reglade3 : MonoBehaviour
     [Header("Dependencies")]
 
     [SerializeField] private TMP_InputField _knoweightOneInputfield;
-    [SerializeField] private TMP_InputField _knoDistanceOneInputfield;
+    [SerializeField] private TMP_InputField _knowDistanceOneInputfield;
     [SerializeField] private TMP_InputField _knoweightTwoInputfield;
-    [SerializeField] private TMP_InputField _knoDistanceTwoInputfield;
+    [SerializeField] private TMP_InputField _knowDistanceTwoInputfield;
     [SerializeField] private TextMeshProUGUI _distanceResultText;
     [SerializeField] private TextMeshProUGUI _weightResultText;
+
+    public void CalculateSolution()
+    {
+        Debug.Log("ButtonPress");
+        if (ValiteInput(_knoweightTwoInputfield))
+        {
+            _distanceResultText.text = CalculateDistance().ToString("F2") + "m"; Debug.Log(CalculateDistance().ToString());
+            
+        }
+        else if (ValiteInput(_knowDistanceTwoInputfield))
+        {
+            _weightResultText.text = CalculateWeight().ToString("F2") + "kg"; Debug.Log(CalculateWeight().ToString());
+        }
+    
+    }
+
+    public float CalculateDistance()
+    {
+        return float.Parse(_knowDistanceOneInputfield.text) * float.Parse(_knoweightOneInputfield.text)/ float.Parse(_knoweightTwoInputfield.text);
+    }
+
+    public float CalculateWeight()
+    {
+        return float.Parse(_knoweightOneInputfield.text) * float.Parse(_knowDistanceOneInputfield.text)/ float.Parse(_knowDistanceTwoInputfield.text);
+    }
+
+    private bool ValiteInput(TMP_InputField inputToValidate)
+    {
+        return inputToValidate.text != "";
+    }
+
+
+
+
+
 }
