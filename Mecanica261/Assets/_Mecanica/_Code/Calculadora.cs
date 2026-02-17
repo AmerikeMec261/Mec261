23597ca169360fb4acd88a1cb90a75957c2be396
RogelioAMERIKE <cdmx3073@amerike.edu.mx>
2026-02-17 12:18:09 -0600

    Calculadora terminada

--- Mecanica261/Assets/_Mecanica/_Code/Calculadora.cs
+++ Mecanica261/Assets/_Mecanica/_Code/Calculadora.cs
@@ -11,4 +11,43 @@ public class Calculadora : MonoBehaviour
     [SerializeField] private TMP_InputField _knowDistanceTwoInputField;
     [SerializeField] private TextMeshProUGUI _distanceResultText;
     [SerializeField] private TextMeshProUGUI _weightResultText;
+
+    public void CalculateSolution()
+    {
+        if (ValiteInput(_knowWeightTwoInputField))
+        {
+            _distanceResultText.text = CalculateDistance().ToString("F2") + " m";
+        }
+        else if (ValiteInput(_knowDistanceTwoInputField))
+        {
+            _weightResultText.text = CalculateWeight().ToString("F2") + " kg";
+
+        }
+    }
+
+    public void ClearAll()
+    {
+        _knowWeightOneInputField.text = "";
+        _knowDistanceOneInputField.text = "";
+        _knowWeightTwoInputField.text = "";
+        _knowDistanceTwoInputField.text = "";
+        _distanceResultText.text = "0 m";
+        _weightResultText.text = "0 N";
+    }
+
+    private float CalculateDistance()
+    {
+        return float.Parse(_knowDistanceOneInputField.text) * float.Parse(_knowWeightOneInputField.text) / float.Parse(_knowWeightTwoInputField.text);
+    }
+    private float CalculateWeight() 
+    {
+        return float.Parse(_knowWeightOneInputField.text) * float.Parse(_knowDistanceOneInputField.text) / float.Parse(_knowDistanceTwoInputField.text);
+    }
+    private bool ValiteInput(TMP_InputField inputToValidate)
+    {
+        return inputToValidate.text != "";
+    }
+
 }
+
+
