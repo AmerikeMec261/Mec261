5566a215c062be3935a58cdbdc1505d97ef57dbd
SrT0RTUGA <cdmx2984@amerike.edu.mx>
2026-02-23 12:28:24 -0600

    calculator force
    chicotio

---
+++ Mecanica261/Assets/fyerza.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class fyerza : MonoBehaviour
+{
+    [Header("Dpendencias")]
+    [SerializeField] private TMP_InputField _weightInputField;
+    [SerializeField] private TMP_InputField _angleInputField;
+    [SerializeField] private TMP_InputField _FrictionInputField;
+    [SerializeField] private TMP_InputField _gravityInputField;
+    [SerializeField] private TextMeshProUGUI _answerLabel;
+
+
+    public void Calculate()
+    {
+        _answerLabel.text = (CalculateParaleWeight() + CalculateNormalForce()).ToString("F2") + "N";
+    }
+
+    public float CalculateWeight()
+    {
+        return float.Parse(_weightInputField.text) * float.Parse(_gravityInputField.text);
+    }
+    private float CalculateParaleWeight()
+    {
+        return CalculateWeight() * Mathf.Sin(float.Parse(_angleInputField.text) * Mathf.Deg2Rad);
+    }
+
+    private float CalculateNormalForce()
+    {
+        return CalculateWeight() * Mathf.Cos(float.Parse(_angleInputField.text) * Mathf.Deg2Rad);
+    }
+
+    private float CalculateFrictionForce()
+    {
+        return CalculateNormalForce() * float.Parse(_FrictionInputField.text);
+    }
+}
