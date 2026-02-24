ce40505a1a2d4d04948d116e87154fb1cdd125b4
EmiCelis <emsace16@gmail.com>
2026-02-23 22:49:32 -0600

    Subiendo actualización de calculadora1

--- Mecanica261/Assets/_Mecanica/_Code/LeverCalculadora.cs
+++ Mecanica261/Assets/_Mecanica/_Code/LeverCalculadora.cs
@@ -1,13 +1,49 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using TMPro.Examples;
 
-public class LeverCalculadora : MonoBehaviour
+public class LevelCalculator : MonoBehaviour
 {
-    [SerializeField] private TMP_InputField _knownWeightOneInputField;
-    [SerializeField] private TMP_InputField _knownDistanceOneInputField;
-    [SerializeField] private TMP_InputField _knownWeightTwoInputField;
-    [SerializeField] private TMP_InputField _knownDistanceTwoInputField;
-    [SerializeField] private TextMeshProUGUI _distanceResultText;
-    [SerializeField] private TextMeshProUGUI _weightResultText;
+    [SerializeField] private TMP_InputField _knowWeigthOneInputField;
+    [SerializeField] private TMP_InputField _knowDistanceOneInputField;
+    [SerializeField] private TMP_InputField _knowWeigthTwoInputField;
+    [SerializeField] private TMP_InputField _knowDistanceTwoInputField;
+    [SerializeField] private TextMeshProUGUI _distanceResult_Text;
+    [SerializeField] private TextMeshProUGUI _weigthResultText;
+
+    public void CalculateSolution()
+    {
+        if (ValiteInput(_knowWeigthTwoInputField))
+        {
+            _distanceResult_Text.text = CalculateDistancie().ToString("F2") + "m";
+        }
+        else if (ValiteInput(_knowDistanceTwoInputField))
+        {
+            _weigthResultText.text = CalculateWeigtn().ToString("F2") + "kg";
+        }
+    }
+    public void ClearAll()
+    {
+        _knowDistanceOneInputField.text = "";
+        _knowDistanceOneInputField.text = "";
+        _knowDistanceTwoInputField.text = "";
+        _knowWeigthTwoInputField.text = "";
+        _weigthResultText.text = "0 m";
+        _distanceResult_Text.text = "0 kg";
+    }
+
+    private float CalculateDistancie()
+    {
+        return float.Parse(_knowDistanceOneInputField.text) * float.Parse(_knowWeigthOneInputField.text) / float.Parse(_knowWeigthTwoInputField.text);
+    }
+
+    private float CalculateWeigtn()
+    {
+        return float.Parse(_knowWeigthOneInputField.text) * float.Parse(_knowDistanceOneInputField.text) / float.Parse(_knowDistanceTwoInputField.text);
+    }
+    private bool ValiteInput(TMP_InputField InputToValide)
+    {
+        return InputToValide.text != "";
+    }
 }
