db45df6e8e53786c56e1776ea638189c816940ec
Fernando747 <fernandojcr5@gmail.com>
2026-02-24 12:40:00 -0600

    Adds parabolic shot functionality
    Implements a `ParabolicShot` script to simulate projectile motion with configurable velocity and angle.
    
    Also, adds a new scene to showcase this functionality.
    Increases the force applied in the "PlanoInclinado" scene to improve the simulation.

---
+++ Mecanica261/Assets/_Mecanica/_Code/TiroParabolico/ParabolicShot.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ParabolicShot : MonoBehaviour
+{
+    [Header("Dependencies")]
+    [SerializeField] private Rigidbody _rigidbody;
+
+    [Header("Dependencies")]
+    [SerializeField] private float _velocity = 20f;
+    [SerializeField] private float _angle = 45f;
+
+    private void Start()
+    {
+        float angleInRadians = _angle * Mathf.Deg2Rad;
+        Vector3 velocityVector = new Vector3(_velocity * Mathf.Cos(angleInRadians), _velocity * Mathf.Sin(angleInRadians), 0f);
+        _rigidbody.linearVelocity = velocityVector;
+    }
+}
