4da3efe2489d8a32db6d5f23022e78d905a6e3ab
ASLIKAAAAAAAAAAAN <cdmx2947@amerike.edu.mx>
2026-02-24 12:43:13 -0600

    Simulador de Fuerza Y Cubo

---
+++ Mecanica261/Assets/Scripts/Cube.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlanoInclinado : MonoBehaviour
+{
+    public float masa = 200f;
+    public float angulo = 20f;
+    public float gravedad = 9.81f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        float fuerza = masa * gravedad * Mathf.Sin(angulo * Mathf.Deg2Rad);
+
+        Vector3 direccion = Vector3.down;
+        direccion = Vector3.ProjectOnPlane(direccion, transform.up).normalized;
+
+        rb.AddForce(direccion * fuerza);
+    }
+}
