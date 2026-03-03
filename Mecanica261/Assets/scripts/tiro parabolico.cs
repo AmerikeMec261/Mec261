bb85f92e23e42b5a8ad3d7ea05f8a584a5b87845
guille <cdmx2939@amerike.edu.mx>
2026-03-03 12:11:21 -0600

    Juego terminado

--- Mecanica261/Assets/scripts/tiro parabolico.cs
+++ Mecanica261/Assets/scripts/tiro parabolico.cs
@@ -4,10 +4,13 @@ public class tiroparabolico : MonoBehaviour
 {
     public Vector3 fuerza = new Vector3(14.14f, 14.14f, 0f);
     Rigidbody rb;
+
+    private Vector3 Inicialposition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
        rb = GetComponent<Rigidbody>();
+        Inicialposition = this.transform.position;
     }
 
     void Update()
@@ -16,6 +19,12 @@ public class tiroparabolico : MonoBehaviour
           {
             rb.AddForce(fuerza, ForceMode.VelocityChange);
           }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rb.linearVelocity = Vector3.zero;
+            this.transform.position = Inicialposition;
+        }
     }
 
    
