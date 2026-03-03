779f3b14a7930cce5a687d12a59b3e29765cac7f
xdIEGUX <cdmx3047@amerike.edu.mx>
2026-03-03 12:05:11 -0600

    juego terminado

--- Mecanica261/Assets/SCRIPTS/Balon.cs
+++ Mecanica261/Assets/SCRIPTS/Balon.cs
@@ -8,14 +8,31 @@ public class Balon : MonoBehaviour
     [Header("Settings")]
     [SerializeField] private float _Fuerza = 20f;  
     [SerializeField] private float _Angulo = 30f;
+    
     private bool lanzamiento = false;
+    private Vector3 Posicion;
+    private Quaternion Rotacion;
    
+
+    private void Awake()
+    {
+        Posicion = transform.position;
+        Rotacion = transform.rotation;
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && !lanzamiento)
         {
             LanzarBalon();
         }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _rigidbody.linearVelocity = new Vector3(0, 0, 0);
+            transform.position = Posicion;
+            transform.rotation = Rotacion;
+        }
+     
     }
 
     private void LanzarBalon()
@@ -25,5 +42,7 @@ public class Balon : MonoBehaviour
         Vector3 Direccion = new Vector3(0f, Mathf.Sin(angleInRadians), Mathf.Cos(angleInRadians));
         Vector3 VelocidadFinal = transform.TransformDirection(Direccion) * _Fuerza;
         _rigidbody.linearVelocity = VelocidadFinal;
+        
     }
+
 }
