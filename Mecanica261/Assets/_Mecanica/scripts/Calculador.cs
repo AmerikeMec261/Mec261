0c02f9f8d8ce365f97320deb28c702e7e704b7da
Giselle Gallegos <gisellegallegos@outlook.com>
2026-03-06 19:20:21 -0600

    primera calculadora
    listo

---
+++ Mecanica261/Assets/_Mecanica/scripts/Calculador.cs
@@ -0,0 +1,53 @@
+using TMPro;
+using UnityEngine;
+
+public class Calculador : MonoBehaviour
+{
+    [Header("Dependencies")]
+    [SerializeField] private TMP_InputField _knowWeightOneInputField;
+    [SerializeField] private TMP_InputField _knowDistanceOneInputField;
+    [SerializeField] private TMP_InputField _knowWeightTwoInputField;
+    [SerializeField] private TMP_InputField _knowDistanceTwoInputField;
+    [SerializeField] private TextMeshProUGUI _distanceResultText;
+    [SerializeField] private TextMeshProUGUI _weightResultText;
+
+    public void CalculateSolution()
+    {
+        if (ValidateInput(_knowWeightTwoInputField))
+        {
+            _distanceResultText.text = CalculateDistance().ToString("F2") + " m";
+
+        }
+        else if (ValidateInput(_knowDistanceTwoInputField))
+        {
+            _weightResultText.text = CalculateWeight().ToString("F2") + " N";
+        }
+    }
+
+    public void ClearAll()
+    {
+        _knowWeightOneInputField.text = "";
+        _knowDistanceOneInputField.text = "";
+        _knowWeightTwoInputField.text = "";
+        _knowDistanceTwoInputField.text = "";
+        _distanceResultText.text = "0 N";
+        _weightResultText.text = "0 N";
+
+
+
+    }
+
+    private float CalculateDistance()
+    {
+        return float.Parse(_knowDistanceOneInputField.text) * float.Parse(_knowWeightOneInputField.text) / float.Parse(_knowWeightTwoInputField.text);
+    }
+
+    private float CalculateWeight()
+    {
+        return float.Parse(_knowWeightOneInputField.text) * float.Parse(_knowDistanceOneInputField.text) / float.Parse(_knowDistanceTwoInputField.text);
+    }
+    private bool ValidateInput(TMP_InputField inputToValidate)
+    {
+        return inputToValidate.text != "";
+    }
+}
