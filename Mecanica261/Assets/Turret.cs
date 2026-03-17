5f1315d72f238193cf6ba515af4d90c269b40cd7
Gael Saucedo <saucedo.gael1612@gmail.com>
2026-03-17 12:57:40 -0600

    avance del cañon

---
+++ Mecanica261/Assets/Turret.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [Header("Dependencies")]
+    [SerializeField] private Transform _yamPivot;
+    [SerializeField] private Transform _pitchPivot;
+    [SerializeField] private Transform _bulletSpawn;
+    [SerializeField] private GameObject _bulletPrefab;
+
+    [Header("Yaw Settings")]
+    [SerializeField] private float _yawSpeed = 90f;
+    [SerializeField] private Vector2 _yawLimits = new Vector2(-90f, 90f);
+
+    [Header("Pitch Settings")]
+    [SerializeField] private float _pitchSpeed = 90f;
+    [SerializeField] private Vector2 _pitchLimits = new Vector2(-10f, 90f);
+
+    public void FireProjectile()
+    {
+        GameObject currentBullet = Instantiate(_bulletPrefab, _bulletSpawn.position, _bulletSpawn.rotation);
+        currentBullet.GetComponent<IProjectile>()?.Fire();
+    }
+
+    private void Update()
+    {
+        float yawInput = Input.GetKey(KeyCode.A) ? -1f : Input.GetKey(KeyCode.D) ? 1f : 0f;
+        float pitchInput = Input.GetKey(KeyCode.W) ? -1f : Input.GetKey(KeyCode.S) ? 1f : 0f;
+
+        RotateYaw(yawInput);
+        RotatePitch(pitchInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FireProjectile();
+        }
+    }
+
+    private void RotateYaw(float input)
+    {
+        float yawChange = input * _yawSpeed * Time.deltaTime;
+        float newYaw = Mathf.Clamp(_yamPivot.localEulerAngles.y + yawChange, _yawLimits.x, _yawLimits.y);
+        _yamPivot.localEulerAngles = new Vector3(_yamPivot.localEulerAngles.x, newYaw, _yamPivot.localEulerAngles.z);
+    }
+
+    private void RotatePitch(float input)
+    {
+        float pitchChange = input * _pitchSpeed * Time.deltaTime;
+        float newPitch = Mathf.Clamp(_pitchPivot.localEulerAngles.z + pitchChange, _pitchLimits.x, _pitchLimits.y);
+        _pitchPivot.localEulerAngles = new Vector3(_pitchPivot.localEulerAngles.x, _pitchPivot.localEulerAngles.y, newPitch);
+    }
+}
