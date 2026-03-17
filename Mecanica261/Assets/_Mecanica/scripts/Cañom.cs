10a361fd85081960fa6ac83ca9e9644ca5f1aab8
Guille Oropeza <cdmx2939@amerike.edu.mx>
2026-03-17 13:04:04 -0600

    Torreta por terminar

---
+++ Mecanica261/Assets/_Mecanica/scripts/Cañom.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Cañom : MonoBehaviour
+{
+    [SerializeField] private Transform _yawPivot;
+    [SerializeField] private Transform _pitchPivot;
+    [SerializeField] private Transform _bulletSpawn;
+    [SerializeField] private Transform _bulletPrefab;
+
+    [Header("Yaw Settings")]
+    [SerializeField] private float _pitchSpeed = 90f;
+    [SerializeField] private Vector2 _pitchLimits = new Vector2(-10f, 90f);
+
+    private void Update()
+    {
+        float yawInput = Input.GetKeyDown(KeyCode.A) ? -1f : Input.GetKeyDown(KeyCode.D) ? 1f : 0f;
+        float pitchInput = Input.GetKeyDown(KeyCode.W) ? 1f : Input.GetKeyDown(KeyCode.S) ? -1f : 0f;
+
+        RotateYaw(yawInput);
+        RotatePitch(pitchInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FireProjectile();
+        }
+
+    }
+
+    private void RotateYaw(float input)
+    {
+        float yawChange = input * _yawSpeed * Time.deltaTime;
+        float newYaw = Mathf.Clamp(_yawPivot.localEulerAngles.y + yawChange, _yawLimits.x, _yawLimits.y);
+        _yawPivot.localEulerAngles = new Vector3(_yawPivot.localEulerAngles.x, newYaw, _yawPivot.localEulerAngles.z);
+    }
+
+    private void RotatePitch(float input)
+    {
+        float pitchChange = input * _pitchSpeed * Time.deltaTime;   
+        float newPitch = Mathf.Clamp(_pitchPivot.localEulerAngles.z + pitchChange, _pitchLimits.x, _pitchLimits.y); 
+        _pitchPivot
+    }
+}
+
