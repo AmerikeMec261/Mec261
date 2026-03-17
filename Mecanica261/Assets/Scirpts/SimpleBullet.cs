e3a93b0ceb163f93c7ea42755edf9ec17e751f9f
Carlos Abarca <jemilio1407@gmail.com>
2026-03-17 12:57:59 -0600

    Tower Defense
    Tower defense 1.0

---
+++ Mecanica261/Assets/Scirpts/SimpleBullet.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+
+public class SimpleBullet : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float _speed = 20f;
+
+    public void Fire()
+    {
+        GetComponent<Rigidbody>().linearVelocity = transform.forward * _speed;
+    }
+}
