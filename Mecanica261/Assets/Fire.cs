46def5403a5d6539b3d9917eec66c253ff74b96b
SrT0RTUGA <cdmx2984@amerike.edu.mx>
2026-03-23 12:42:06 -0600

    gytvuvgy

--- Mecanica261/Assets/Fire.cs
+++ Mecanica261/Assets/Fire.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class SimpleBullet: MonoBehaviour , Iprojectile
+public class SimpleBullet: MonoBehaviour , IProjectile
 {
-
     [Header("Settings")]
+
+    [SerializeField] private float _speed = 200.0f;
+    public void Fire()
+    {
+        throw new System.NotImplementedException();
+    }
 
 
 
