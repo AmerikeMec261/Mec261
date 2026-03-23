e112e179c281e5abc0ab23e14f82e3680cc2da5b
DieguxTY <cdmx3047@amerike.edu.mx>
2026-03-23 12:45:24 -0600

    torreta completa

--- Mecanica261/Assets/SCRIPTS/SimpleBullet.cs
+++ Mecanica261/Assets/SCRIPTS/SimpleBullet.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
-public class SimpleBullet : MonoBehaviour
+public class SimpleBullet : MonoBehaviour,IProjectile
 {
     [Header("Settings")]
-    [SerializeField] private float _speed = 20f;
+    [SerializeField] private float _speed = 25f;
 
     public void Fire()
     {
