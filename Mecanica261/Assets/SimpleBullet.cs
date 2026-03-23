58cadbf45a43ac0427065ac66b82632f9dadd1d6
Gael Saucedo <saucedo.gael1612@gmail.com>
2026-03-23 12:32:43 -0600

    cañon terminado

--- Mecanica261/Assets/SimpleBullet.cs
+++ Mecanica261/Assets/SimpleBullet.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 [RequireComponent (typeof(Rigidbody))]
 
-public class SimpleBullet : MonoBehaviour
+public class SimpleBullet : MonoBehaviour, IProjectile
 {
     [Header("Settings")]
-    [SerializeField] private float _speed = 20f;
+    [SerializeField] private float _speed = 60f;
 
     public void Fire()
     {
