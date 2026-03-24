95280af8a21ca2fba5d51b68e18409a97280ec0d
Emi PAdilla <emsace16@gmail.com>
2026-03-24 12:52:10 -0600

    Subiendo cambios de rotación

--- Mecanica261/Assets/_Mecanica/_Code/TiroHyperbolico.cs
+++ Mecanica261/Assets/_Mecanica/_Code/TiroHyperbolico.cs
@@ -1,72 +1,83 @@
 using UnityEngine;
-using UnityEngine.InputSystem;  
+using UnityEngine.InputSystem;
 
 public class TiroHyperbolico : MonoBehaviour
 {
     [Header("Datos iniciales")]
-    public float initialVelocity = 20f;   
-    public float angle = 45f;            
-    public float gravity = 9.8f;          
+    public float initialVelocity = 20f;
+    public float angle = 45f;
+    public float gravity = 9.8f;
     private float v0x;
     private float v0y;
     private float _flyingTime;
     private float _time;
-    private Vector3 _startPosition;  
+    private Vector3 _startPosition;
     private bool _isShooting = false;
-    private PlayerInput _playerInput;  
+    private PlayerInput _playerInput;
 
     void Awake()
-    {   
-       var espacioAction = new InputAction("Disparar", InputActionType.Button, "<Keyboard>/space");
-       espacioAction.performed += _ => IniciarDisparo();
-       espacioAction.Enable();
+    {
+        var espacioAction = new InputAction("Disparar", InputActionType.Button, "<Keyboard>/space");
+        espacioAction.performed += _ => IniciarDisparo();
+        espacioAction.Enable();
     }
     void Start()
     {
-       _startPosition = transform.position;
-
-        
-       float anguloRad = angle * Mathf.Deg2Rad;
+        _startPosition = transform.position;
 
 
-       v0x = initialVelocity * Mathf.Cos(anguloRad);
-       v0y = initialVelocity * Mathf.Sin(anguloRad);
+        float anguloRad = angle * Mathf.Deg2Rad;
 
-        
-       _flyingTime = (2 * v0y) / gravity;
 
-       Debug.Log("V0x = " + v0x);
-       Debug.Log("V0y = " + v0y);
-       Debug.Log("Tiempo = " + _flyingTime + " s");
+        v0x = initialVelocity * Mathf.Cos(anguloRad);
+        v0y = initialVelocity * Mathf.Sin(anguloRad);
+
+
+        _flyingTime = (2 * v0y) / gravity;
+
+        Debug.Log("V0x = " + v0x);
+        Debug.Log("V0y = " + v0y);
+        Debug.Log("Tiempo = " + _flyingTime + " s");
     }
 
     void Update()
     {
         if (!_isShooting)
         {
-           _time = 0f;  
-           return;
+            _time = 0f;
+            return;
         }
 
         _time += Time.deltaTime;
-       
+
         float x = v0x * _time;
         float y = v0y * _time - 0.5f * gravity * _time * _time;
 
         transform.position = _startPosition + new Vector3(x, y, 0);
- 
+
         if (y < 0)
         {
             _isShooting = false;
         }
-    }   
+    }
     private void IniciarDisparo()
     {
         if (!_isShooting)
         {
             _isShooting = true;
             _time = 0f;
-            Debug.Log("¡Disparo iniciado con espacio!");
+            Debug.Log("Disparo iniciado con espacio");
         }
+    }
+
+    private void AnguloDeteccion()
+    {
+      float anguloRad = angle * Mathf.Deg2Rad;
+        v0x = initialVelocity * Mathf.Cos(anguloRad);
+        v0y = initialVelocity * Mathf.Sin(anguloRad);
+        _flyingTime = (2 * v0y) / gravity;
+        Debug.Log("V0x = " + v0x);
+        Debug.Log("V0y = " + v0y);
+        Debug.Log("Tiempo = " + _flyingTime + " s");
     }
 }
