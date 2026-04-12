c29429dd5f6ae2c363e65349c27786d36c2e4859
Fernando747 <fernandojcr5@gmail.com>
2026-04-12 14:06:42 -0600

    Revision de exámen

--- Mecanica261/Assets/Scripts/Segundo Parcial/TowerDefence/Scripts/Turret_3.cs
+++ Mecanica261/Assets/Scripts/Segundo Parcial/TowerDefence/Scripts/Turret_3.cs
@@ -18,7 +18,7 @@ public class Turret_3 : MonoBehaviour
     [SerializeField] private float _projectileSpeed = 30f;
     [SerializeField] private float _minDistance = 1f;
     [SerializeField] private bool _useHighArc = false;
-    
+
     private Vector3 _targetPoint;
     private bool _hasSolution;
 
@@ -33,14 +33,14 @@ public class Turret_3 : MonoBehaviour
         }
 
         // Cambiar de Bullet
-       if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-             _currentBulletIndex = 0;
+            _currentBulletIndex = 0;
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-             _currentBulletIndex = 1;
+            _currentBulletIndex = 1;
         }
 
     }
@@ -72,9 +72,9 @@ public class Turret_3 : MonoBehaviour
     {
         Vector3 origin = _bulletSpawn.position;
 
-        Vector3 dir = _targetPoint - _yawPivot.position;
+        Vector3 dir = _targetPoint - _yawPivot.position; //recuerda no usar abreviaciones
 
-        Vector3 flatDir = new Vector3(dir.x, 0f, dir.z);
+        Vector3 flatDir = new Vector3(dir.x, 0f, dir.z); //recuerda no usar abreviaciones
 
         float distance = flatDir.magnitude;
 
@@ -86,7 +86,7 @@ public class Turret_3 : MonoBehaviour
         // yaw
         if (flatDir.sqrMagnitude > 0.001f)
         {
-            Quaternion rot = Quaternion.LookRotation(flatDir);
+            Quaternion rot = Quaternion.LookRotation(flatDir); //recuerda no usar abreviaciones
             _yawPivot.rotation = Quaternion.Euler(0f, rot.eulerAngles.y, 0f);
         }
 
@@ -107,11 +107,11 @@ public class Turret_3 : MonoBehaviour
     // El disparo 
     private void FireProjectile()
     {
-        GameObject bullet = Instantiate( _bulletPrefabs[_currentBulletIndex], _bulletSpawn.position, _bulletSpawn.rotation);
+        GameObject bullet = Instantiate(_bulletPrefabs[_currentBulletIndex], _bulletSpawn.position, _bulletSpawn.rotation);
 
-        IProjectile proj = bullet.GetComponent<IProjectile>();
+        IProjectile proj = bullet.GetComponent<IProjectile>(); //recuerda no usar abreviaciones
 
-        if (proj != null)
+        if (proj != null) // puedes cambiar el null check por ? depues del GetComponent
         {
             proj.SetSpeed(_projectileSpeed);
             proj.Fire();
@@ -119,7 +119,7 @@ public class Turret_3 : MonoBehaviour
     }
 
     // Parabólico / Fórumla de la clase del Martess
-    private bool SolveBallisticAngle(Vector3 origin, Vector3 target, float speed, out float angle)
+    private bool SolveBallisticAngle(Vector3 origin, Vector3 target, float speed, out float angle) //recuerda no usar abreviaciones
     {
         float g = Physics.gravity.magnitude;
 
@@ -148,4 +148,6 @@ public class Turret_3 : MonoBehaviour
 
         return true;
     }
+
+    //Ejercicio en clase: cambia el método de arriba para no usar abrevaciones o varibales "mágicas", tienes que saber a qué se refiere X, G, etc. y poner nombres que lo reflejen. Por ejemplo, en vez de "x" podrías usar "horizontalDistance", en vez de "g" podrías usar "gravity", etc.
 }
