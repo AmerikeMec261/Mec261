7c38f0bac7d30b7e42943ec5abe357021e62f699
Fernando747 <fernandojcr5@gmail.com>
2026-04-20 12:39:27 -0600

    Adds a simple buoyancy system and physics-based water test scene
    Implements a `SimpleFloat` component that applies upward buoyant force to rigidbodies based on water level, volume, and density. Includes a test scene with floating cube configurations to verify the physics behavior.

---
+++ Mecanica261/Assets/_Mecanica/_Code/WaterTest/SimpleFloat.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SimpleFloat : MonoBehaviour
+{
+    [SerializeField] private float _waterLevel = 0;
+    [SerializeField] private float _volume = 1;
+    [SerializeField] private float _waterDensity = 1000;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Float();
+    }
+
+    private void Float()
+    {
+        float summersion = Mathf.Clamp01((_waterLevel - transform.position.y / 1f));
+
+        if (summersion < 0f) return;
+
+        float force = _waterDensity * summersion * _volume * Physics.gravity.magnitude;
+
+        _rigidbody.AddForce(Vector3.up * force, ForceMode.Force);
+    }
+}
