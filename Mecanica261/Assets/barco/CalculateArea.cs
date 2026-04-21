755a66724302d3562632fa7b0fff483be66e94d8
weapache <cdmx2984@amerike.edu.mx>
2026-04-21 12:49:24 -0600

    yamato

---
+++ Mecanica261/Assets/barco/CalculateArea.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+
+public class CalculateArea : MonoBehaviour
+{
+    private float CalculateArea()
+    {
+        float area = 0f;
+
+        for (int i = 0; i < _floatPoints.Count; i++)
+        {
+            Vector3 current = transform.InverseTransformPoint(_floatPoints[i].position);
+            Vector3 next = transform.InverseTransformPoint(_floatPoints[(i + 1) % _floatPoints.Count].position);
+
+            area += (current.x * next.z) - (next.x * current.z);
+        }
+
+        return Mathf.Abs(area) * 0.5f;
+    }
+}
