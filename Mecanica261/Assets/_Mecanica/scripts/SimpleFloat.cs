ecaab1a126db768399d73c0fa68cefe42eb66079
Guille Oropeza <cdmx2939@amerike.edu.mx>
2026-04-21 12:56:42 -0600

    Barco+Inicio de codigo

--- Mecanica261/Assets/_Mecanica/scripts/SimpleFloat.cs
+++ Mecanica261/Assets/_Mecanica/scripts/SimpleFloat.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class SimpleFloat : MonoBehaviour
@@ -5,8 +6,17 @@ public class SimpleFloat : MonoBehaviour
     [SerializeField] private float _waterlevel = 0;
     [SerializeField] private float _volume = 1;
     [SerializeField] private float _waterdensity = 1000;
+    [SerializeField] private float _ShapeFactor;
+    [SerializeField] private Transform _topPoint;
+    [SerializeField] private Transform _bottomPoint;
+    [SerializeField] private Transform[] _floatPoints;
+    [SerializeField] private Rigidbody _rigidbody;
 
-    private Rigidbody _rigidbody;
+    [SerializeField] private float Area;
+    [SerializeField] private float HullHeight;
+    [SerializeField] private float HullVolume;
+    [SerializeField] private float Draft;
+
 
     private void Awake()
     {
@@ -19,6 +29,8 @@ public class SimpleFloat : MonoBehaviour
         Float();
     }
 
+  
+
     private void Float()
     {
         float summersion = Mathf.Clamp01((_waterlevel - transform.position.y / 1f));
@@ -30,4 +42,17 @@ public class SimpleFloat : MonoBehaviour
         _rigidbody.AddForce(Vector3.up * force, ForceMode.Force);
     }
 
+  
+
+    private void FloatShip()
+    {
+
+    }
+
+    private void CalculateHullData()
+    {
+
+    }
+
+    [SerializeField] private float CalculateArea;
 }
