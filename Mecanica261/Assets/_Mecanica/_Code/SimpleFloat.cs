b424fc8a2e563b5fb77e8f72ad892a965716231c
Emi PAdilla <emsace16@gmail.com>
2026-04-21 12:49:48 -0600

    Subiendo esqueleto y modelo

--- Mecanica261/Assets/_Mecanica/_Code/SimpleFloat.cs
+++ Mecanica261/Assets/_Mecanica/_Code/SimpleFloat.cs
@@ -1,16 +1,36 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class SimpleFloat : MonoBehaviour
 {
+    [Header("Water Settings")]
     [SerializeField] private float _waterLevel = 0;
     [SerializeField] private float _volume = 1;
     [SerializeField] private float _waterDensity = 1000;
 
-    private Rigidbody _rigbody;
+    [Header("Physics Settings")]
+    [SerializeField] private float _shapeFactor;
+    [SerializeField] private Transform _topPoint;
+    [SerializeField] private Transform _bottomPoint;
+    [SerializeField] private List<Transform> _floatPoints;
+    [SerializeField] private Rigidbody _rigidbody;
+
+    [Header("Debug Settings")]
+    [SerializeField] private float area;
+    [SerializeField] private float hullHeight;
+    [SerializeField] private float hullVolume;
+    [SerializeField] private float draft;
 
     private void Awake()
     {
-        _rigbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+
+    private void awake()
+    {
 
     }
 
@@ -27,6 +47,32 @@ public class SimpleFloat : MonoBehaviour
 
         float force = _waterDensity * summersion * Physics.gravity.magnitude;
 
-        _rigbody.AddForce(Vector3.up * force * _volume, ForceMode.Force);
+        _rigidbody.AddForce(Vector3.up * force * _volume, ForceMode.Force);
     }
+
+    private void FloatShip()
+    {
+
+    }
+
+    private void CalculateHullData()
+    {
+
+    }
+
+    private float CalculateArea()
+    {
+        float area = 0f;
+
+        for (int i = 0; i < _floatPoints.Count; i++)
+        {
+            Vector3 current = transform.InverseTransformPoint(_floatPoints[i].position);
+            Vector3 next = transform.InverseTransformPoint(_floatPoints[(i + 1) % _floatPoints.Count].position);
+
+            area += (current.x * next.z) - (next.x * current.z);
+        }
+
+        return Mathf.Abs(area) * 0.5f;
+    }
+
 }
