0a7563ec180b1291f88e084b9762299f6f61001b
Gael Saucedo <saucedo.gael1612@gmail.com>
2026-04-27 13:11:15 -0600

    barco caido

--- Mecanica261/Assets/AguaPhysc.cs
+++ Mecanica261/Assets/AguaPhysc.cs
@@ -1,16 +1,102 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine.ProBuilder;
+using JetBrains.Annotations;
 
+[RequireComponent(typeof(Rigidbody))]
 public class AguaPhysc : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Water")]
+    [SerializeField] private float _waterLevel = 0f;
+    [SerializeField] private float _waterDensity = 1000f; 
+    [SerializeField] private float _waterDraft = 10f;
+
+    [Header("BattleShip")]
+    [SerializeField] private float _shapeFactor_ = 1.0f;
+    [SerializeField] private Transform _topPoint;
+    [SerializeField] private Transform _bottomPoint;
+    [SerializeField] private List<Transform> _floatPoints;
+
+    private float _area;
+    private float _HullHeight;
+    private float _HullVolume;
+
+
+    private Rigidbody _rigidbody;
+
+    public float Area { get; }
+    public float HullHeight { get; }
+    public float HullVolume { get; }
+    public float Graft { get; }
+
+
+
+    private void Awake()
+    {
+        
+        _rigidbody = GetComponent<Rigidbody>();
+        CalculateHullData();
+    }
+
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        FloatShip();
+    }
+
+    private void FloatShip()
+    {
+        float gravity = Physics.gravity.magnitude;
+        float volumePerPoint = HullVolume /  _floatPoints.Count;
         
+        for (int i = 0; i < _floatPoints.Count; i++)
+        {
+           Transform point = _floatPoints[i];
+            float submersion = Mathf.Clamp01(_waterLevel - point.position.y) / _HullHeight;
+
+            if (submersion <= 0)
+            {
+                continue;
+            }
+            float force = _waterDensity * volumePerPoint * gravity * submersion;
+            _rigidbody.AddForceAtPosition(Vector3.up * force , point.position, ForceMode.Force);
+
+            Vector3 velocity = _rigidbody.GetPointVelocity(point.position);
+            
+            _rigidbody. AddForceAtPosition(-velocity * _waterDraft * submersion, point.position, ForceMode.Force);
+        }
+    }
+
+    private void CalculateHullData()
+    {
+        _area = CalculateArea();
+        _HullHeight = _topPoint.position.y - _bottomPoint.position.y;
+        _HullVolume = _area * _HullHeight * _shapeFactor_;
+
+        float requireVolume = _rigidbody.mass / _waterDensity;
+        _waterDraft = requireVolume / (_area * _shapeFactor_);
+
+
+
+    }
+
+    
+    private float CalculateArea()
+    {
+        float area = 0f;
+
+        for (int i = 0; i < _floatPoints.Count; i++)
+        {
+            Vector3 current = transform.InverseTransformPoint(_floatPoints[i].position);
+            Vector3 next = transform.InverseTransformPoint(_floatPoints[(i + 1) % _floatPoints.Count].position);
+
+            area += (current.x * next.z) - (next.x * current.z);
+        }
+
+        return Mathf.Abs(area) * 0.5f;
     }
 }
