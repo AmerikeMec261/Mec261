eae99af043a794f53261a41955f2b6f4bd4c506e
JavierAlvarez <jemilio1407@gmail.com>
2026-05-04 09:29:44 -0600

    Subiendo el cambio de bala por tecla
    El traqueo esta a medias

--- Mecanica261/Assets/Scirpts/TurretBarco.cs
+++ Mecanica261/Assets/Scirpts/TurretBarco.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 using System.Collections.Generic;
 using NaughtyAttributes;
 using Unity.VisualScripting;
+using UnityEngine.Rendering;
 
 public class TurretBarco : MonoBehaviour
 {
-    [Header("Turret")]
-    [SerializeField] private Transform _target;
-    [SerializeField] private Transform _turret;
-
     [Header("Cannon parts")]
+    [SerializeField] private Transform towerOrigin;
     [SerializeField] private Transform picth;
     [SerializeField] private Transform yaw;
     [SerializeField] private Transform bulletExit;
@@ -23,23 +21,104 @@ public class TurretBarco : MonoBehaviour
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float rotationDegrees;
 
+    [Header("Tracking")]
+    [SerializeField] private float trackingRange = 50f;
+    [SerializeField] private string enemyTag = "Enemy";
+
+    private int currentBullet = 0;
+    private bool enemyInRange = false;
+    private Transform _target;
+
     void Start()
     {
-        
+        SelectWeapon();    
     }
     
     void Update()
     {
-        Rotation(); 
+        ChangeBullet();
+    }
+
+    private void FixedUpdate()
+    {
+        Rotation();
     }
 
     private void Rotation()
     {
-        Vector3 direction = _target.position - _turret.position;
-        direction.y = 0f;
+        if(_target != null)
+        {
+            float distance = Vector3.Distance(transform.position, _target.position);
 
-        float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            if(distance > trackingRange || !_target.gameObject.activeInHierarchy)
+            {
+                _target = null;
+                enemyInRange = false;
+            }
+            else
+            {
+                enemyInRange = true;
+            }
+        }
+    }
 
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+    private void FireBullet()
+    {
+
+    }
+
+    private void MathShoot()
+    {
+
+    }
+
+    private void ChangeBullet()
+    {
+        int previousBullet = currentBullet;
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (currentBullet >= bullets.Count - 1)
+            {
+                currentBullet = 0;
+            }
+            else
+            {
+                currentBullet++;
+            }
+        }
+
+        if(previousBullet != currentBullet)
+        {
+            SelectWeapon();
+        }
+
+    }
+
+    private void SelectWeapon()
+    {
+        if(bullets.Count > 0 && bullets[currentBullet] != null)
+        {
+            bulletSpeed = bullets[currentBullet].GetComponent<IProjectile>().Speed;
+            print("Bala cambiada" + bullets[currentBullet].name);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {        
+        if (bulletExit != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(bulletExit.position, bulletExit.forward * 5f);
+        }
+        
+
+        if (yaw != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(yaw.position, trackingRange);
+        }
     }
 }
+
+//Para el cambio de balas use este video
+//https://www.youtube.com/watch?v=Dn_BUIVdAPg
