d25ad2b1db3f40832084d73717997e1fc8498ae1
EmiCelis <emsace16@gmail.com>
2026-05-08 22:54:54 -0600

    Subiendo guias de estudio pipipi

--- Mecanica261/Assets/_Mecanica/_Code/PhysicsHelper.cs
+++ Mecanica261/Assets/_Mecanica/_Code/PhysicsHelper.cs
@@ -1,115 +1,117 @@
 using UnityEngine;
 
 
-    public static class PhysicsHelper
+public static class PhysicsHelper
+{
+
+    // Se aplica una fuerza para llegar a la velocidad indicada mientras se compensa con aceleracion vectorial
+
+    public static void ApplyForceToReachVelocity(Rigidbody rigidbody, Vector3 velocity, float force = 1, ForceMode mode = ForceMode.Force)
     {
+        if (force == 0 || velocity.magnitude == 0)
+            return;
 
-        // Se aplica una fuerza para llegar a la velocidad indicada mientras se compensa con aceleracion vectorial
+        // Se compensa la resistencia del drag normalizandola y multiplicando por el damping linear
+        velocity = velocity + velocity.normalized * 0.2f * rigidbody.linearDamping;
 
-        public static void ApplyForceToReachVelocity(Rigidbody rigidbody, Vector3 velocity, float force = 1, ForceMode mode = ForceMode.Force)
-        {
-            if (force == 0 || velocity.magnitude == 0)
-                return;
-
-            // Se compensa la resistencia del drag normalizandola y multiplicando por el damping linear
-            velocity = velocity + velocity.normalized * 0.2f * rigidbody.linearDamping;
-
-            //force = 1 => necesita 1 para llegar a la velocidad (Si la masa es 1 1) => force puede ser maximo 1 / Time.fixedDeltaTime (referencia a la guia)
-            force = Mathf.Clamp(force, -rigidbody.mass / Time.fixedDeltaTime, rigidbody.mass / Time.fixedDeltaTime);
+        //force = 1 => necesita 1 para llegar a la velocidad (Si la masa es 1 1) => force puede ser maximo 1 / Time.fixedDeltaTime (referencia a la guia)
+        force = Mathf.Clamp(force, -rigidbody.mass / Time.fixedDeltaTime, rigidbody.mass / Time.fixedDeltaTime);
 
         // El producto escalar es una proyección del lado derecho al lado izquierdo con una longitud igual a resultado / magnitud del lado izquierdo https://www.youtube.com/watch?v=h0NJK4mEIJU
         // Se aplican fuerzas dependiendo la proyeccion del objetivo
         if (rigidbody.linearVelocity.magnitude == 0)
-            {
-                rigidbody.AddForce(velocity * force, mode);
-            }
-            else
-            {
-                var velocityProjectedToTarget = (velocity.normalized * Vector3.Dot(velocity, rigidbody.linearVelocity) / velocity.magnitude);
-                rigidbody.AddForce((velocity - velocityProjectedToTarget) * force, mode);
-            }
-        }
-
-
-        // Control de rotacion en X usando RPS
-        public static void ApplyTorqueToReachRPS(Rigidbody rigidbody, Quaternion rotation, float rps, float force = 1)
         {
-            var radPerSecond = rps * 2 * Mathf.PI + rigidbody.angularDamping * 20;
-
-            float angleInDegrees;
-            Vector3 rotationAxis;
-            rotation.ToAngleAxis(out angleInDegrees, out rotationAxis);
-
-            if (force == 0 || rotationAxis == Vector3.zero)
-                return;
-
-            rigidbody.maxAngularVelocity = Mathf.Max(rigidbody.maxAngularVelocity, radPerSecond);
-
-            force = Mathf.Clamp(force, -rigidbody.mass * 2 * Mathf.PI / Time.fixedDeltaTime, rigidbody.mass * 2 * Mathf.PI / Time.fixedDeltaTime);
-
-            var currentSpeed = Vector3.Project(rigidbody.angularVelocity, rotationAxis).magnitude;
-
-            rigidbody.AddTorque(rotationAxis * (radPerSecond - currentSpeed) * force);
+            rigidbody.AddForce(velocity * force, mode);
         }
-
-        // La conversion de rotacion a velocidad angular
-        public static Vector3 QuaternionToAngularVelocity(Quaternion rotation)
+        else
         {
-            float angleInDegrees;
-            Vector3 rotationAxis;
-            rotation.ToAngleAxis(out angleInDegrees, out rotationAxis);
-
-            return rotationAxis * angleInDegrees * Mathf.Deg2Rad;
-        }
-
-        public static Quaternion AngularVelocityToQuaternion(Vector3 angularVelocity)
-        {
-            var rotationAxis = (angularVelocity * Mathf.Rad2Deg).normalized;
-            float angleInDegrees = (angularVelocity * Mathf.Rad2Deg).magnitude;
-
-            return Quaternion.AngleAxis(angleInDegrees, rotationAxis);
-        }
-
-        // Calculo normal y centro del plano sobre su superficie
-        public static Vector3 GetNormal(Vector3[] points)
-        {
-            //https://www.ilikebigbits.com/2015_03_04_plane_from_points.html
-            if (points.Length < 3)
-                return Vector3.up;
-
-            var center = GetCenter(points);
-
-            float xx = 0f, xy = 0f, xz = 0f, yy = 0f, yz = 0f, zz = 0f;
-
-            for (int i = 0; i < points.Length; i++)
-            {
-                var r = points[i] - center;
-                xx += r.x * r.x;
-                xy += r.x * r.y;
-                xz += r.x * r.z;
-                yy += r.y * r.y;
-                yz += r.y * r.z;
-                zz += r.z * r.z;
-            }
-
-            var det_x = yy * zz - yz * yz;
-            var det_y = xx * zz - xz * xz;
-            var det_z = xx * yy - xy * xy;
-
-            if (det_x > det_y && det_x > det_z)
-                return new Vector3(det_x, xz * yz - xy * zz, xy * yz - xz * yy).normalized;
-            if (det_y > det_z)
-                return new Vector3(xz * yz - xy * zz, det_y, xy * xz - yz * xx).normalized;
-            else
-                return new Vector3(xy * yz - xz * yy, xy * xz - yz * xx, det_z).normalized;
-
-        }
-
-        public static Vector3 GetCenter(Vector3[] points)
-        {
-            var center = Vector3.zero;
-            for (int i = 0; i < points.Length; i++)
-                center += points[i] / points.Length;
-            return center;
+            var velocityProjectedToTarget = (velocity.normalized * Vector3.Dot(velocity, rigidbody.linearVelocity) / velocity.magnitude);
+            rigidbody.AddForce((velocity - velocityProjectedToTarget) * force, mode);
         }
     }
+
+
+    // Control de rotacion en X usando RPS
+    public static void ApplyTorqueToReachRPS(Rigidbody rigidbody, Quaternion rotation, float rps, float force = 1)
+    {
+        var radPerSecond = rps * 2 * Mathf.PI + rigidbody.angularDamping * 20;
+
+        float angleInDegrees;
+        Vector3 rotationAxis;
+        rotation.ToAngleAxis(out angleInDegrees, out rotationAxis);
+
+        if (force == 0 || rotationAxis == Vector3.zero)
+            return;
+
+        rigidbody.maxAngularVelocity = Mathf.Max(rigidbody.maxAngularVelocity, radPerSecond);
+
+        force = Mathf.Clamp(force, -rigidbody.mass * 2 * Mathf.PI / Time.fixedDeltaTime, rigidbody.mass * 2 * Mathf.PI / Time.fixedDeltaTime);
+
+        var currentSpeed = Vector3.Project(rigidbody.angularVelocity, rotationAxis).magnitude;
+
+        rigidbody.AddTorque(rotationAxis * (radPerSecond - currentSpeed) * force);
+    }
+
+    // La conversion de rotacion a velocidad angular
+    public static Vector3 QuaternionToAngularVelocity(Quaternion rotation)
+    {
+        float angleInDegrees;
+        Vector3 rotationAxis;
+        rotation.ToAngleAxis(out angleInDegrees, out rotationAxis);
+
+        return rotationAxis * angleInDegrees * Mathf.Deg2Rad;
+    }
+
+    public static Quaternion AngularVelocityToQuaternion(Vector3 angularVelocity)
+    {
+        var rotationAxis = (angularVelocity * Mathf.Rad2Deg).normalized;
+        float angleInDegrees = (angularVelocity * Mathf.Rad2Deg).magnitude;
+
+        return Quaternion.AngleAxis(angleInDegrees, rotationAxis);
+    }
+
+    // Calculo normal y centro del plano sobre su superficie
+    public static Vector3 GetNormal(Vector3[] points)
+    {
+        //https://www.ilikebigbits.com/2015_03_04_plane_from_points.html
+        if (points.Length < 3)
+            return Vector3.up;
+
+        var center = GetCenter(points);
+
+        float xx = 0f, xy = 0f, xz = 0f, yy = 0f, yz = 0f, zz = 0f;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            var r = points[i] - center;
+            xx += r.x * r.x;
+            xy += r.x * r.y;
+            xz += r.x * r.z;
+            yy += r.y * r.y;
+            yz += r.y * r.z;
+            zz += r.z * r.z;
+        }
+
+        var det_x = yy * zz - yz * yz;
+        var det_y = xx * zz - xz * xz;
+        var det_z = xx * yy - xy * xy;
+
+        if (det_x > det_y && det_x > det_z)
+            return new Vector3(det_x, xz * yz - xy * zz, xy * yz - xz * yy).normalized;
+        if (det_y > det_z)
+            return new Vector3(xz * yz - xy * zz, det_y, xy * xz - yz * xx).normalized;
+        else
+            return new Vector3(xy * yz - xz * yy, xy * xz - yz * xx, det_z).normalized;
+
+    }
+
+    public static Vector3 GetCenter(Vector3[] points)
+    {
+        var center = Vector3.zero;
+        for (int i = 0; i < points.Length; i++)
+            center += points[i] / points.Length;
+        return center;
+    }
+
+    //https://youtu.be/yEGdYM0nk1I?si=sBrR5CXw02C8gDeO
+}
