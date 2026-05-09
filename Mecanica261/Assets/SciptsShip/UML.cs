05aeaa546e3d46969b7967153973466caeef156b
LA MAMI ASKANNNNNNN <cdmx2947@amerike.edu.mx>
2026-05-08 23:40:57 -0600

    umL

---
+++ Mecanica261/Assets/SciptsShip/UML.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class UML : MonoBehaviour
+{
+    [Header("Water")]
+    [SerializeField] private float _waterLevel = 0f;
+    [SerializeField] private float _waterDensity = 1000f;
+    [SerializeField] private float _verticalWaterDrag = 1f;
+    [SerializeField] private float _horizontalWaterDrag = 0.005f;
+
+    [Header("Hull")]
+    [SerializeField] private float _shapeFactor = 0.67f;
+    [SerializeField] private Transform _topPoint;
+    [SerializeField] private Transform _bottomPoint;
+    [SerializeField] private List<Transform> _floatPoints = new List<Transform>();
+
+
+    private Rigidbody _rigidbody;
+
+    private float _area;
+    private float _hullHeight;
+    private float _hullVolume;
+    private float _draft;
+
+ 
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        CalculateHullData();
+    }
+
+    private void FixedUpdate()
+    {
+        FloatShip();
+    }
+
+
+    private void FloatShip()
+    {
+        float gravity = Physics.gravity.magnitude;
+        float volumePerPoint = _hullVolume / _floatPoints.Count;
+
+        for (int i = 0; i < _floatPoints.Count; i++)
+        {
+            Transform point = _floatPoints[i];
+
+            float submersion = Mathf.Clamp01((_waterLevel - point.position.y) / _hullHeight);
+            if (submersion <= 0f) { continue; }
+
+            float force = _waterDensity * volumePerPoint * gravity * submersion;
+            _rigidbody.AddForceAtPosition(Vector3.up * force, point.position, ForceMode.Force);
+
+            Vector3 velocity = _rigidbody.GetPointVelocity(point.position);
+
+            Vector3 verticalVelocity = Vector3.Project(velocity, Vector3.up);
+            Vector3 horizontalVelocity = velocity - verticalVelocity;
+
+            Vector3 verticalDrag = -verticalVelocity * verticalVelocity.magnitude * _verticalWaterDrag * submersion;
+            Vector3 horizontalDrag = -horizontalVelocity * horizontalVelocity.magnitude * _horizontalWaterDrag * submersion;
+
+            _rigidbody.AddForceAtPosition(verticalDrag + horizontalDrag, point.position, ForceMode.Force);
+        }
+    }
+
+    private void CalculateHullData()
+    {
+        _area = CalculateAreaXZ();
+        _hullHeight = _topPoint.position.y - _bottomPoint.position.y;
+        _hullVolume = _area * _hullHeight * _shapeFactor;
+
+        float requiredVolume = _rigidbody.mass / _waterDensity;
+        _draft = requiredVolume / (_area * _shapeFactor);
+    }
+
+    private float CalculateAreaXZ()
+    {
+        float area = 0f;
+
+        for (int i = 0; i < _floatPoints.Count; i++)
+        {
+            Vector3 current = transform.InverseTransformPoint(_floatPoints[i].position);
+            Vector3 next = transform.InverseTransformPoint(_floatPoints[(i + 1) % _floatPoints.Count].position);
+
+            area += (current.x * next.z) - (next.x * current.z);
+        }
+
+        return Mathf.Abs(area) * 0.5f;
+    }
+
+}
