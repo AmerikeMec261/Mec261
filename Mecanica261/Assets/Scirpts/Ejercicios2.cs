50055b3863420e08a502e83e72f2002f5f56c70e
Cazador457 <jemilio1407@gmail.com>
2026-05-19 12:49:44 -0600

    Subiendo metodos
    Subiendo los primeros metodos hechos.

---
+++ Mecanica261/Assets/Scirpts/Ejercicios2.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Ejercicios2 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    //public int 
+}
