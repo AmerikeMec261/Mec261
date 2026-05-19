ce510b1a272b71783317e4a70f82d6f328bd51dc
Guille Oropeza <cdmx2939@amerike.edu.mx>
2026-05-19 12:55:48 -0600

    Subiendo avance de ejercicios2

---
+++ Mecanica261/Assets/Scripts ejercicios/Ejercicios 2.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Ejercicios2 : MonoBehaviour
+{
+    [SerializeField] private int _playerHealth = 100f;
+    [SerializeField] private int _damage = 5f;
+
+
+    private override void PlayerDamage() // ejercicio 1
+    {
+        _playerHealth -= _damage
+    }
+
+    private override void PlayerAlive(bool player Alive)
+    {
+        if (_playerhealth <0 
+    }
+
+}
