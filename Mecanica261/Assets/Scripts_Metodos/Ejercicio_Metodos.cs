b913227b2af637f1d54bf6175cae15cfaf710c21
Calef <cdmx3126@amerike.edu.mx>
2026-05-19 12:55:14 -0600

    2do ejercicios

---
+++ Mecanica261/Assets/Scripts_Metodos/Ejercicio_Metodos.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Ejercicio_Metodos : MonoBehaviour
+{
+
+
+    public void ReceiveDamage(int Damage)//Ej.1
+    {
+        
+    }
+
+    public bool IsPlayerAlive(int PlayerHealth)//Ej.2
+    {
+        return PlayerHealth>0;   
+    }
+
+    public void Distance(Vector3 Position1,Vector3 Position2)
+    {
+        
+    }
+}
