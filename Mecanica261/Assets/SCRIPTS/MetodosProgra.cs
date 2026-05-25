816bad05a5f495cf829d1fb5f369d50d2c188613
dieguxty <cdmx3047@amerike.edu.mx>
2026-05-25 01:10:42 -0600

    2do Ejercicio completo

--- Mecanica261/Assets/SCRIPTS/MetodosProgra.cs
+++ Mecanica261/Assets/SCRIPTS/MetodosProgra.cs
@@ -3,39 +3,116 @@ using UnityEngine;
 
 public class MetodosProgra : MonoBehaviour
 {
-    public int CurrentHealth = 100;
-    public string PlayerName = "Diegux";
+    //public int CurrentHealth = 100;
+    //public string PlayerName = "Diegux";
+    //public RigidBody RigidBody;
 
-   
-    
-    
-    
-    
-    
-    public void TakeDamage(int damage)
-    {
-        CurrentHealth -= damage;
-    }
 
-    public bool IsAlive()
-    {
-        return false;
-    }
 
-    public float PlayerDistance(Vector3 positionA, Vector3 positionB)
-    {
-        return 0f;
-    }
-    
-    public Vector3 GetDirection(Vector3 origin, Vector3 Destiny)
-    {
-        return (Destiny - origin);
-    } 
 
-    public string PlayerName2()
-    {
-        return PlayerName;
-    }
+
+
+
+    //public void TakeDamage(int damage)
+    //{
+    //CurrentHealth -= damage;
+    //}
+
+    //public bool IsAlive()
+    //{
+    //return false;
+    //}
+
+    //public float PlayerDistance(Vector3 positionA, Vector3 positionB)
+    //{
+    //return 0f;
+    //}
+
+    //public Vector3 GetDirection(Vector3 origin, Vector3 Destiny)
+    //{
+    //return (Destiny - origin);
+    //} 
+
+    // public string PlayerName2()
+    //{
+    // return PlayerName;
+    //}
+
+    // public int EnemyCount(List<GameObject>enemies)
+    //{
+    // return enemies;
+    //}
+
+    //public GameObject ClosestEnemy(Vector3 PlayerPosition, List<GameObject> enemies)
+    //{
+    // return null;
+    //}
+
+    // public void PlayerMovement(Vector3 Direction, float Speed)
+    //{
+    // transform.position += Direction * Speed;
+    //}
+
+    //public float DegreesToRadians(float Degrees)
+    //{
+    //  return Degrees * Mathf.Deg2Rad;
+    //}
+
+    //public bool ClosestPlayer(List<GameObject> Players)
+    //{
+    // return Players;
+    //}
+
+    //public bool ConvertToInt(string Text, out int Number)
+    //{
+    //return int.TryParse(Text, out Number);
+    //}
+
+    //public QuaternionRotation(float Angle)
+    //{
+    //return Quaternion.Euler(Angle);
+    //}
+
+    //public void EnemiesInRange(Vector3 Center, float Radius, List<GameObject> Enemies)
+    //{
+    //return;
+    //}
+
+    //public void ResePosition(Vector3 Spawn)
+    //{
+    //transform.position = Spawn;
+    //}
+
+    //public virtual void Attack()
+    //{
+    // Debug.Log("El enemigo esta atacando");
+    //}
+
+    //public override void Attack()
+    //{
+    //Debug.Log("El enemigo esta atacando");
+    //}
+
+    //public override void Attack()
+    //{
+    //base.Attack();
+    //Debug.Log("El jefe final ya no tine vida");
+    //}
+
+    //public float HealthPorcentaje(float Health, float MaxHealth)
+    //{
+    // return Health / MaxHealth;
+    //}
+
+    //public bool TryDodge(float Dodge, float Random)
+    //{
+    //return Random <= Dodge;
+    //}
+
+    //public void ApplyForce(Vector3 Direction, float Force)
+    //{
+    //RigidBody.AddForce(Direction * Force);
+    //}
 
 
 
