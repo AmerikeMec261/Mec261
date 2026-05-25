989f32eaa394dd29ac2cf6dfe95081c6f3114721
Giselle Gallegos <gisellegallegos@outlook.com>
2026-05-24 21:20:23 -0600

    ejercicios 1 y 2 subidos

---
+++ Mecanica261/Assets/_Mecanica/scripts/Ejercicios1.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Ejercicios1 : MonoBehaviour
+{/*
+   1. private int _velocity = 5;
+2. public int PlayerLevel;
+3. public int BaseDamage = 10;
+4. private int _playerHealth;
+5. private Rigidbody _rigidbody;
+6. private bool _isAlive;
+7. internal int _saveIndex;
+
+    [Range(0, 100)]
+8. [SerializeField] private int _attackRange; 
+9. public string PlayerName;
+10. protected int _movementVelocity;
+11. private MeshRenderer _meshRenderer;
+
+12. [Tooltip("esta variable controla el volumen")]
+    public int Volume;
+13. private bool _canAttack;
+14. public static GameManager Instance;
+15. private List<Item> _items = new List<Item>();
+
+16. [FormerlySerializedAs("_speed")]
+    public int Velocity;
+17. private Vector3 _playerPosition;
+18. public int MaxPlayer;
+19. [SerializeField] protected int _distance;
+20. [SerializeField] private AudioSource _audioSource;
+    */
+}
---
+++ Mecanica261/Assets/_Mecanica/scripts/Ejercicios2.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class Ejercicios2 : MonoBehaviour
+{
+   /*
+    1. public void ReduceHealth (int damageAmount)
+   {
+   _currentHealth -= damageAmount;
+   }
+    
+    2. public bool IsPlayerAlive()
+   {
+   return _currentHealth > 0;
+   }
+
+    3.
+
+    4.
+
+    5. public string GetPlayerName ()
+   {
+   return _playerName;
+   }
+
+    6. public int GetEnemyCount (List<GameObject> enemies)
+   {
+   
+   }
+
+    7.
+
+    8.
+
+    9.
+
+    10.
+
+    11. public bool TryParseInt (string text, out int value)
+   {
+   return int.TryParse(text, out value);
+   }
+
+    12.
+
+    13.
+
+    14. public void ResetPlayerPosition (Vector3 spawnPosition)
+   {
+   transform.position = spawnPosition;
+   }
+
+    15. public virtual void Attack ()
+   {
+   
+   }
+
+    16. public override void Attack ()
+   {
+
+   }
+
+    17. public override void Attack ()
+   {
+   base.Attack();
+   }
+
+    18.
+
+    19.
+
+    20.
+
+    */
+}
