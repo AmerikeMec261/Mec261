bcbc9efa120b5e4cb6882e0ebeb4723c79f5a918
Giselle Gallegos <gisellegallegos@outlook.com>
2026-05-25 20:31:47 -0600

    ejercicios3 listo, ejercicios 2 no puedo

--- Mecanica261/Assets/_Mecanica/scripts/Ejercicios3.cs
+++ Mecanica261/Assets/_Mecanica/scripts/Ejercicios3.cs
@@ -1,5 +1,7 @@
+using NUnit.Framework;
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Ejercicios3 : MonoBehaviour
 {
@@ -11,6 +13,9 @@ public class Ejercicios3 : MonoBehaviour
     [SerializeField] public int AttackDistance;
     [SerializeField] private int _volume;
     [SerializeField] private float _speed;
+    [SerializeField] private List<string> _inventoryItems;
+    [SerializeField] private int _maxInventory;
+    [SerializeField] private Rigidbody _rigidbody;
     public int Health {get; private set;}
 
     public bool IsDead => Health <= 0; 
@@ -50,7 +55,19 @@ public class Ejercicios3 : MonoBehaviour
 
     public DateTime CharacterCreationDate { get; } = DateTime.Now;
 
-    //public bool IsInventoryFull => _inventoryItems.Count >= _maxInventory;
+    public bool IsInventoryFull => _inventoryItems.Count >= _maxInventory;
+
+    //public int MaxLevel { get; init;}
+
+    //public float HorizontalSpeed => _rigidbody.velocity;  
+
+    public float Energy { get; private set; }
+
+    public Vector3 CurrentPosition => transform.position;
+
+    //public InventoryItems => _inventoryItems;
+
+    //public bool IsRunning => _rigidbody.velocity => _runSpeed;
 
     
 
