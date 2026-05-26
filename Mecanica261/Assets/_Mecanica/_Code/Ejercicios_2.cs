b13e9e8d85d76a31b256f1f6cfbc445ae91986ab
al3-4 <cdmx2995@amerike.edu.mx>
2026-05-25 22:29:52 -0600

    Ejercicio 3 terminado

--- Mecanica261/Assets/_Mecanica/_Code/Ejercicios_2.cs
+++ Mecanica261/Assets/_Mecanica/_Code/Ejercicios_2.cs
@@ -6,6 +6,14 @@ public class Ejercicios_2 : MonoBehaviour
 {
     private float _currentHealth, _maxHealth;
     private int _damage;
+    [SerializeField] private int _stamina;
+    private int _distancePlayer;
+    private int _rangeAttack;
+    private List<GameObject> _slots;
+    private int _maxSlots;
+    private Rigidbody _rigidBody;
+    private float _runSpeed;
+
     public int Health { get; private set; }// 1
 
     public bool IsDead => Health <= 0;// 2
@@ -25,5 +33,26 @@ public class Ejercicios_2 : MonoBehaviour
 
     public int Gameobject { get; private set; }// 9
 
+    public int Stamina { get => _stamina; set => _stamina = value; }// 10
 
+    public bool CanAttack => _distancePlayer <= _rangeAttack;// 11
+
+    public float _volumen;
+    public float Volumen { get { return _volumen; } set { _volumen = Mathf.Clamp(value, 0f, 100f); } }// 12
+
+    public string CreationCharacter { get; } = "25/05/2026";// 13
+
+    public bool InventoryFull => _slots.Count >= _maxSlots;// 14
+
+    //public int MexLevel { get; init; }// 15
+
+    public float HorizontalSpeed => _rigidBody.linearVelocity.x;// 16
+
+    public int Energy { get; private set; }// 17
+
+    public Vector3 CurrentPosition => transform.position;// 18
+
+    public List <GameObject> Slots => _slots;// 19
+
+    public bool IsRunning => _speed > _runSpeed;// 20
 }
