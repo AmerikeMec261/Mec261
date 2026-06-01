676d034488e1aa870078a206347cae2a4e9cf760
Fernando747 <fernandojcr5@gmail.com>
2026-05-31 20:31:39 -0600

    Minecraft Sounds

--- Mecanica261/Assets/_Mecanica/_Code/Minecraft/Projectile.cs
+++ Mecanica261/Assets/_Mecanica/_Code/Minecraft/Projectile.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Minecraft
 {
     [RequireComponent(typeof(Rigidbody))]
     public class Projectile : MonoBehaviour, IAttacker
     {
+        [Header("Attack")]
         [SerializeField] private float _damage = 10f;
+
+        [Header("Events")]
+        [SerializeField] private UnityEvent _onFired = new UnityEvent();
+        [SerializeField] private UnityEvent _onHit = new UnityEvent();
 
         private Rigidbody _rigidbody;
 
         public float Damage { get { return _damage; } }
+        public UnityEvent OnFired { get { return _onFired; } }
+        public UnityEvent OnHit { get { return _onHit; } }
 
         private void Awake()
         {
@@ -20,6 +28,7 @@ namespace Minecraft
         {
             _damage = damage;
             _rigidbody.linearVelocity = direction.normalized * speed;
+            _onFired.Invoke();
         }
 
         private void OnCollisionEnter(Collision collision)
@@ -27,6 +36,7 @@ namespace Minecraft
             IDamagable damagable = collision.collider.GetComponentInParent<IDamagable>();
 
             damagable?.ReceiveDamage(Damage);
+            _onHit.Invoke();
 
             Destroy(gameObject);
         }
