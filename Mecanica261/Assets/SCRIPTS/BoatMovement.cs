4e50eddec419d921af54c283104b1567e1984003
dieguxty <cdmx3047@amerike.edu.mx>
2026-06-01 21:41:09 -0600

    Correccion de examen

--- Mecanica261/Assets/SCRIPTS/BoatMovement.cs
+++ Mecanica261/Assets/SCRIPTS/BoatMovement.cs
@@ -24,16 +24,16 @@ public class BoatMovement : MonoBehaviour
 
     private void Move()
     {
-        if (_engine == null) return; //<- Posible uso de IA. Verificar que el componente esté asignado en el inspector para evitar errores en tiempo de ejecución.
         float vertical = Input.GetAxis("Vertical");
-        if (_rigidbody.linearVelocity.magnitude > _maxSpeed) return; //Debes limitar la velocidad con una correcta aplicación de fuerza
-        Vector3 force =_engine.forward * vertical * _moveForce * Time.deltaTime; 
-        _rigidbody.AddForceAtPosition(force, _engine.position);
+        if (_rigidbody.linearVelocity.magnitude < _maxSpeed)
+        {
+            Vector3 force = transform.forward * vertical * _moveForce * Time.deltaTime;
+            _rigidbody.AddForceAtPosition(force, _engine.position);
+        }
     }
 
     private void Turn()
     {
-        if (_rudder == null) return;  //<- Posible uso de IA. Verificar que el componente esté asignado en el inspector para evitar errores en tiempo de ejecución.
         float horizontal = Input.GetAxis("Horizontal");
         Vector3 force = transform.right * horizontal * _turnForce * Time.deltaTime; //Buena aplicación de fuerza
         _rigidbody.AddForceAtPosition(force, _rudder.position);
